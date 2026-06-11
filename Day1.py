b21156c7c9212a4c493985ec52460d1c2cf59aae
Shwetajha-206 <shwetajh9999@gmail.com>
2026-06-11 12:07:16 +0530

    Add multiple functions for user input and calculations

--- Day1.py
+++ Day1.py
@@ -121,3 +121,46 @@ def main():
 def square(n):
     return n*n
 main()
+#IndoorVoice
+s = input("What's your name? ")
+print(s.lower())
+
+#Playback speed
+
+s = input()
+print(s.replace(" ", "..."))
+
+#Making Faces
+
+def convert(s):
+    s = s.replace(":)", "🙂")
+    s = s.replace(":(", "🙁")
+    return s
+
+def main():
+    s = input()
+    print(convert(s))
+
+main()
+
+#Einstien
+
+m = int(input())
+c = 300000000
+E = m * c ** 
+print(E)
+
+#Tip Calculator
+def main():
+    dollars = dollars_to_float(input("How much was the meal? "))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
+    tip = dollars * percent
+    print(f"Leave ${tip:.2f}")
+
+def dollars_to_float(d):
+    return float(d[1:])
+
+def percent_to_float(p):
+    return float(p[:-1]) / 100
+
+main()
