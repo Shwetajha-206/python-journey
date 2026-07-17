4478195b300447f78f0e409ee709e121ba2f2335
Shweta Jha <shwetajh9999@gmail.com>
2026-07-17 13:03:11 +0530

    day 4 update

--- Day2.py
+++ Day2.py
@@ -57,67 +57,67 @@ y = int(input("What's y? "))
 
 if x==y:
     print("X is equal to y")
-#else:
-    #print("X is not equal to y")
+else:
+    print("X is not equal to y")
 
 #and
-#score = int(input("Score: "))
+score = int(input("Score: "))
 
-#if score>=90 and score<=100:
-    #print("Grade: A")
-#elif score>=80 and score<90:
-    #print("Grade: B")
-#elif score>=70 and score<80:
-   # print("Grade: C")
-#elif score>=60 and score<70:
-    #print("Grade: D")
-#else:
-    #print("Grade: F")
+if score>=90 and score<=100:
+    print("Grade: A")
+elif score>=80 and score<90:
+    print("Grade: B")
+elif score>=70 and score<80:
+    print("Grade: C")
+elif score>=60 and score<70:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
 
 #Still, we can further improve our program:
-#score = int(input("Score: "))
+score = int(input("Score: "))
 
-#if score >= 90:
-    #print("Grade: A")
-#elif score >= 80:
-    #print("Grade: B")
-#elif score >= 70:
-    #print("Grade: C")
-#elif score >= 60:
-    #print("Grade: D")
-#else:
-    #print("Grade: F")
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+elif score >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
 
-#x = int(input("What's X? "))
-#if x % 2 == 0:
-    #print("Even")
-#else:
-    #print("odd")
+x = int(input("What's X? "))
+if x % 2 == 0:
+    print("Even")
+else:
+    print("odd")
 
 #Creating your own parity function
-#def main():
-    #x = int(input("What's X? "))
-    #if is_even(x):
-        #print("Even")
-    #else:
-        #print("odd")
+def main():
+    x = int(input("What's X? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("odd")
 
-#def is_even(n):
-    #if n%2 == 0:
-        #return True
-    #else:
-       # return False
-#main()
+def is_even(n):
+    if n%2 == 0:
+        return True
+    else:
+        return False
+main()
 
 #Pythonic
 
-#def main():
-    #x = int(input("What's x? "))
-    #if is_even(x):
-        #print("Even")
-    #else:
-        #print("Odd")
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
 
 def is_even(n):
